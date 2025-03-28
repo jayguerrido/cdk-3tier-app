b524c6f09ba40ec1f927ec0dc89defb9020af311
Jonathan Guerrido <johnnyblitzem@icloud.com>
2025-03-28 18:44:43 +0000

    save the lib and bin stack

---
+++ bin/cdk-3tier-app.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { Cdk3TierAppStack } from '../lib/cdk-3tier-app-stack';
+
+const app = new cdk.App();
+new Cdk3TierAppStack(app, 'Cdk3TierAppStack');
+
+  }
+}
---
+++ lib/cdk-3tier-app-stack.ts
@@ -0,0 +1,19 @@
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class Cdk3TierAppStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // VPC
+    const vpc = new ec2.Vpc(this, 'AppVPC', { maxAzs: 2 });
+
+    // EC2
+    new ec2.Instance(this, 'WebServer', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: ec2.MachineImage.latestAmazonLinux(),
+    });
+  }
+}
