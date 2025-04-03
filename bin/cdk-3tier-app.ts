0dbb5c005544a0b6ff8b656c44d41f1a0ec80bcc
Jonathan Guerrido <johnnyblitzem@icloud.com>
2025-04-03 00:45:21 +0000

    SAVE EVERYTHING

--- bin/cdk-3tier-app.ts
+++ bin/cdk-3tier-app.ts
@@ -3,4 +3,10 @@ import * as cdk from 'aws-cdk-lib';
 import { Cdk3TierAppStack } from '../lib/cdk-3tier-app-stack';
 
 const app = new cdk.App();
-new Cdk3TierAppStack(app, 'Cdk3TierAppStack');
+
+new Cdk3TierAppStack(app, 'Cdk3TierAppStack', {
+  env: {
+    account: process.env.AWS_ACCOUNT_ID,
+    region: process.env.AWS_DEFAULT_REGION,
+  },
+});
